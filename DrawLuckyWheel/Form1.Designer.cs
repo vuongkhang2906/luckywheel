c2728ec54e0bdab49ded5121a79f08b26c9d6244
vuongkhang2906 <gaogao2962002@gmail.com>
2023-07-12 22:23:52 +0700

    init project

---
+++ DrawLuckyWheel/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DrawLuckyWheel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.Color.Green;
+            this.btnPlay.Location = new System.Drawing.Point(626, 259);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(200, 132);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(696, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "00";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DrawLuckyWheel.Properties.Resources.placeholder;
+            this.pictureBox2.Location = new System.Drawing.Point(258, 86);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(57, 52);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DrawLuckyWheel.Properties.Resources.lucky_wheel;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 144);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(626, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 666);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "[C#] Lucky Game - https://laptrinhvb.net";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DrawLuckyWheel/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLuckyWheel
+{
+    public partial class Form1 : Form
+    {
+        bool wheelIsMoved;    
+        float wheelTimes;      
+        Timer wheelTimer;
+        LuckyCirlce koloFortuny;
+        public Form1()
+        {
+            InitializeComponent();         
+            wheelTimer = new Timer();
+            wheelTimer.Interval = 30; // speed 
+            wheelTimer.Tick += wheelTimer_Tick;
+            koloFortuny = new LuckyCirlce();
+           
+        }
+        public class LuckyCirlce
+        {
+            public Bitmap obrazek;
+            public Bitmap tempObrazek;
+            public float kat;
+            public int[] wartosciStanu;
+            public int stan;
+
+            public LuckyCirlce()
+            {
+                tempObrazek = new Bitmap(Properties.Resources.lucky_wheel);
+                obrazek = new Bitmap(Properties.Resources.lucky_wheel);
+                wartosciStanu = new int[] { 12, 11, 10 ,09, 08, 07, 06, 05, 04, 03, 02, 01};
+                kat = 0.0f;
+            }
+
+        }
+ 
+        public static Bitmap RotateImage(Image image, float angle)
+        {
+            return RotateImage(image, new PointF((float)image.Width / 2, (float)image.Height / 2), angle);
+        }
+
+        public static Bitmap RotateImage(Image image, PointF offset, float angle)
+        {
+            if (image == null)
+                throw new ArgumentNullException("image");
+
+          
+            Bitmap rotatedBmp = new Bitmap(image.Width, image.Height);
+            rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);         
+            Graphics g = Graphics.FromImage(rotatedBmp);         
+            g.TranslateTransform(offset.X, offset.Y);           
+            g.RotateTransform(angle);       
+            g.TranslateTransform(-offset.X, -offset.Y);      
+            g.DrawImage(image, new PointF(0, 0));
+
+            return rotatedBmp;
+        }
+
+        private void RotateImage(PictureBox pb, Image img, float angle)
+        {
+            if (img == null || pb.Image == null)
+                return;
+
+            Image oldImage = pb.Image;
+            pb.Image = RotateImage(img, angle);
+            if (oldImage != null)
+            {
+                oldImage.Dispose();
+            }
+        }
+        private void wheelTimer_Tick(object sender, EventArgs e)
+        {          
+
+            if (wheelIsMoved && wheelTimes > 0)
+            {
+                koloFortuny.kat += wheelTimes / 10;
+                koloFortuny.kat = koloFortuny.kat % 360;
+                RotateImage(pictureBox1, koloFortuny.obrazek, koloFortuny.kat);
+                wheelTimes--;
+            }
+
+            koloFortuny.stan = Convert.ToInt32(Math.Ceiling(koloFortuny.kat / 30));
+
+            if (koloFortuny.stan == 0)
+            {
+                koloFortuny.stan = 0;
+            }
+            else
+            {
+                koloFortuny.stan -= 1;
+            }
+       
+            label1.Text = Convert.ToString(koloFortuny.wartosciStanu[koloFortuny.stan]);
+
+           
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            wheelIsMoved = true;
+            Random rand = new Random();
+            wheelTimes = rand.Next(150, 200);    //random số vòng quay     
+         
+            wheelTimer.Start();
+        }
+
+      
+    }
+}
