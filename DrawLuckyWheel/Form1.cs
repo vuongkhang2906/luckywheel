78787c3a767253f3fe60eae2d09715622e05cbf9
Quan <dongquan15041@gmail.com>
2023-07-14 19:27:36 +0700

    feat: add a message box after random

--- DrawLuckyWheel/Form1.cs
+++ DrawLuckyWheel/Form1.cs
@@ -13,18 +13,17 @@ namespace DrawLuckyWheel
 {
     public partial class Form1 : Form
     {
-        bool wheelIsMoved;    
-        float wheelTimes;      
+        bool wheelIsMoved;
+        float wheelTimes;
         Timer wheelTimer;
         LuckyCirlce koloFortuny;
         public Form1()
         {
-            InitializeComponent();         
+            InitializeComponent();
             wheelTimer = new Timer();
-            wheelTimer.Interval = 30; // speed 
+            wheelTimer.Interval = 10; // speed 
             wheelTimer.Tick += wheelTimer_Tick;
             koloFortuny = new LuckyCirlce();
-           
         }
         public class LuckyCirlce
         {
@@ -38,12 +37,12 @@ namespace DrawLuckyWheel
             {
                 tempObrazek = new Bitmap(Properties.Resources.lucky_wheel);
                 obrazek = new Bitmap(Properties.Resources.lucky_wheel);
-                wartosciStanu = new int[] { 12, 11, 10 ,09, 08, 07, 06, 05, 04, 03, 02, 01};
+                wartosciStanu = new int[] { 12, 11, 10, 09, 08, 07, 06, 05, 04, 03, 02, 01 };
                 kat = 0.0f;
             }
 
         }
- 
+
         public static Bitmap RotateImage(Image image, float angle)
         {
             return RotateImage(image, new PointF((float)image.Width / 2, (float)image.Height / 2), angle);
@@ -54,13 +53,13 @@ namespace DrawLuckyWheel
             if (image == null)
                 throw new ArgumentNullException("image");
 
-          
+
             Bitmap rotatedBmp = new Bitmap(image.Width, image.Height);
-            rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);         
-            Graphics g = Graphics.FromImage(rotatedBmp);         
-            g.TranslateTransform(offset.X, offset.Y);           
-            g.RotateTransform(angle);       
-            g.TranslateTransform(-offset.X, -offset.Y);      
+            rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+            Graphics g = Graphics.FromImage(rotatedBmp);
+            g.TranslateTransform(offset.X, offset.Y);
+            g.RotateTransform(angle);
+            g.TranslateTransform(-offset.X, -offset.Y);
             g.DrawImage(image, new PointF(0, 0));
 
             return rotatedBmp;
@@ -79,14 +78,27 @@ namespace DrawLuckyWheel
             }
         }
         private void wheelTimer_Tick(object sender, EventArgs e)
-        {          
-
+        {
             if (wheelIsMoved && wheelTimes > 0)
             {
                 koloFortuny.kat += wheelTimes / 10;
                 koloFortuny.kat = koloFortuny.kat % 360;
                 RotateImage(pictureBox1, koloFortuny.obrazek, koloFortuny.kat);
                 wheelTimes--;
+            }
+            else
+            {
+                wheelTimer.Stop();
+            }
+
+            if (!wheelTimer.Enabled)
+            {
+                DialogResult result = MessageBox.Show("we got the result is " + Convert.ToString(koloFortuny.wartosciStanu[koloFortuny.stan]) + ", want to delete this result?", "Notification", MessageBoxButtons.YesNo);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    // TODO: remove an item from circle
+                    //koloFortuny.wartosciStanu = koloFortuny.wartosciStanu.Where(val => val != koloFortuny.wartosciStanu[koloFortuny.stan]).ToArray();
+                }
             }
 
             koloFortuny.stan = Convert.ToInt32(Math.Ceiling(koloFortuny.kat / 30));
@@ -99,21 +111,18 @@ namespace DrawLuckyWheel
             {
                 koloFortuny.stan -= 1;
             }
-       
+
             label1.Text = Convert.ToString(koloFortuny.wartosciStanu[koloFortuny.stan]);
-
-           
         }
-
         private void btnPlay_Click(object sender, EventArgs e)
         {
             wheelIsMoved = true;
             Random rand = new Random();
             wheelTimes = rand.Next(150, 200);    //random số vòng quay     
-         
+
             wheelTimer.Start();
         }
 
-      
+
     }
 }
